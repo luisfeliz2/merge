2d38b8420e17805c85c99b76898ba17424a31a28
luisfeliz2 <luisfelizbeltre@gmail.com>
2023-02-10 10:07:53 +0100

    calculadora

---
+++ Calculadora.java
@@ -0,0 +1,35 @@
+package merge;
+
+
+	
+
+public class Calculadora {
+	
+	public static int sumar(int a, int b) {
+		return a + b;	
+		
+		
+	}
+	
+	public static int restar(int a, int b) {
+		return a - b;
+		
+		
+	}
+	
+	public static int dividir(int a, int b) {
+		return a/b;
+		
+		
+	}
+	
+	public static int multiplicar(int a, int b) {
+		return a*b;
+		
+		
+	}
+
+}
+	
+
+
